87f6c64e0f2e042eb2cdcbb15b15117cf209fa42
Vinícius Sato de Oliveira <vyniciussato@gmail.com>
2024-11-04 05:46:00 -0300

    exe feito

--- UrbanFarming.Desktop/FormListaPedidos.cs
+++ UrbanFarming.Desktop/FormListaPedidos.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using UrbanFarming.Domain.Classes;
+﻿using UrbanFarming.Domain.Classes;
 using UrbanFarmingWeb.UI.Request;
 
 namespace UrbanFarmingDesktop.UI
@@ -12,6 +6,7 @@ namespace UrbanFarmingDesktop.UI
     public partial class FormListaPedidos : Form
     {
         private readonly RequestAPI _requestAPI;
+        private List<PedidoViewModel> _pedidos;
 
         public FormListaPedidos()
         {
@@ -19,18 +14,65 @@ namespace UrbanFarmingDesktop.UI
             var httpClient = new HttpClient();
             _requestAPI = new RequestAPI(httpClient);
             CarregarPedidos();
+            dataGridViewPedidos.CellClick += DataGridViewPedidos_CellClick;
         }
 
         private async void CarregarPedidos()
         {
-            var pedidos = await ObterPedidos();
-            dataGridViewPedidos.DataSource = pedidos;
+            _pedidos = await ObterPedidos();
+            var displayList = _pedidos.Select(p => new
+            {
+                p.CodigoPedido,
+                p.ValorTotal,
+                p.Usuario,
+                p.Data
+            }).ToList();
+
+            dataGridViewPedidos.DataSource = displayList;
         }
 
-        private async Task<List<Pedido>> ObterPedidos()
+        private async Task<List<PedidoViewModel>> ObterPedidos()
         {
-            // Chama a API para obter a lista de pedidos
-            return await _requestAPI.ListaPedidos();
+            var pedidos = await _requestAPI.ListaPedidos();
+            var pedidoViewModels = new List<PedidoViewModel>();
+
+            foreach (var pedido in pedidos)
+            {
+                pedidoViewModels.Add(new PedidoViewModel
+                {
+                    CodigoPedido = pedido.CodigoPedido,
+                    ValorTotal = pedido.ValorTotal,
+                    Usuario = pedido.Usuario,
+                    Data = pedido.Data,
+                    Itens = pedido.Itens ?? new List<ItensPedido>()
+                });
+            }
+
+            return pedidoViewModels;
         }
+
+        private void DataGridViewPedidos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var selectedPedido = _pedidos[e.RowIndex];
+                ShowItensPedido(selectedPedido);
+            }
+        }
+
+        private void ShowItensPedido(PedidoViewModel pedido)
+        {
+            var itensForm = new FormItensPedido(pedido.Itens);
+            itensForm.ShowDialog();
+        }
+    }
+
+    public class PedidoViewModel
+    {
+        public int CodigoPedido { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string Usuario { get; set; }
+        public DateTime Data { get; set; }
+        public List<ItensPedido> Itens { get; set; }
     }
 }
