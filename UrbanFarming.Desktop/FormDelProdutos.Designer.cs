778f0a1fbaac7f998de2b627ae910a5278cc8e13
Vinícius Sato de Oliveira <vyniciussato@gmail.com>
2024-11-06 05:14:54 -0300

    deletar produtos

--- UrbanFarming.Desktop/FormDelProdutos.Designer.cs
+++ UrbanFarming.Desktop/FormDelProdutos.Designer.cs
@@ -2,38 +2,50 @@
 {
     partial class FormDelProdutos
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.Button btnDeletar;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormDelProdutos";
+            dgvProdutos = new DataGridView();
+            btnDeletar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvProdutos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvProdutos
+            // 
+            dgvProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProdutos.Location = new Point(14, 14);
+            dgvProdutos.Margin = new Padding(4, 3, 4, 3);
+            dgvProdutos.Name = "dgvProdutos";
+            dgvProdutos.Size = new Size(887, 404);
+            dgvProdutos.TabIndex = 0;
+            // 
+            // btnDeletar
+            // 
+            btnDeletar.Location = new Point(14, 438);
+            btnDeletar.Margin = new Padding(4, 3, 4, 3);
+            btnDeletar.Name = "btnDeletar";
+            btnDeletar.Size = new Size(117, 35);
+            btnDeletar.TabIndex = 1;
+            btnDeletar.Text = "Deletar";
+            btnDeletar.UseVisualStyleBackColor = true;
+            btnDeletar.Click += btnDeletar_Click;
+            // 
+            // FormDelProdutos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Green;
+            ClientSize = new Size(915, 486);
+            Controls.Add(btnDeletar);
+            Controls.Add(dgvProdutos);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "FormDelProdutos";
+            Text = "Deletar Produtos";
+            Load += FormDelProdutos_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvProdutos).EndInit();
+            ResumeLayout(false);
         }
-
-        #endregion
     }
 }
